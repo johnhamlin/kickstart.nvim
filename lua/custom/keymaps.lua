091843fcb948b9326e25d36a600cb9a6113946ab
John Hamlin <johnhamlin@gmail.com>
2025-03-31 21:59:21 -0400

    Add shortcut to add blank lines

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,6 +2,9 @@
 vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Go to next buffer', silent = true })
 vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Go to previous buffer', silent = true })
 
+vim.keymap.set('n', '<Enter>', 'o<Esc>', { desc = 'Add blank line below', silent = true })
+vim.keymap.set('n', '<S-Enter>', 'O<Esc>', { desc = 'Add blank line below', silent = true })
+
 -- Add any other custom keymaps here
 
 -- Return the module (optional)

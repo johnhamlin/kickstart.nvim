226a82735453f75c9a6d1bb7a7072bedbd6b72d9
John Hamlin <johnhamlin@gmail.com>
2025-06-05 10:36:24 -0400

    Add keymaps to complete statement

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,9 +2,14 @@
 -- vim.keymap.set('n', '<Tab>', ':bnext<CR>', { desc = 'Go to next buffer', silent = true })
 -- vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { desc = 'Go to previous buffer', silent = true })
 
-vim.keymap.set("n", "<Enter>", "o<Esc>", { desc = "Add blank line below", silent = true })
-vim.keymap.set("n", "<S-Enter>", "O<Esc>", { desc = "Add blank line below", silent = true })
+vim.keymap.set('n', '<Enter>', 'o<Esc>', { desc = 'Add blank line below', silent = true })
+vim.keymap.set('n', '<S-Enter>', 'O<Esc>', { desc = 'Add blank line below', silent = true })
 
+vim.keymap.set('i', '<C-;>', '<Esc>A;<CR>', { desc = 'Complete statement with ; and new line' })
+vim.keymap.set('n', '<C-;>', 'A;<CR>', { desc = 'Complete statement with ; and new line' })
+
+vim.keymap.set('i', '<C-,>', '<Esc>A,<CR>', { desc = 'Complete statement with , and new line' })
+vim.keymap.set('n', '<C-,>', 'A,<CR>', { desc = 'Complete statement with , and new line' })
 -- Add any other custom keymaps here
 
 -- Return the module (optional)

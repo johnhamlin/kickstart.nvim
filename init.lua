69502cb36a353847e473813bc501b1051f14ae2f
John Hamlin <johnhamlin@gmail.com>
2025-06-06 15:10:49 -0400

    preserve line breaks in yaml

--- init.lua
+++ init.lua
@@ -833,6 +833,15 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        formatters = {
+          yamlfmt = {
+            args = {
+              '--retain_line_breaks_single',
+              '--retain_line_breaks',
+              '-',
+            },
+          },
+        },
       },
     },
   },

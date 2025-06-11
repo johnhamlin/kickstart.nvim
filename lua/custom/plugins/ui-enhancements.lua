bc3c390556e8f9bbc3c92d10b891800aff9fe78e
John Hamlin <johnhamlin@gmail.com>
2025-06-11 10:49:09 -0400

    Turn on bottom search

--- lua/custom/plugins/ui-enhancements.lua
+++ lua/custom/plugins/ui-enhancements.lua
@@ -147,7 +147,7 @@ return {
         backend = 'nui',
       },
       presets = {
-        -- bottom_search = true,
+        bottom_search = true,
         command_palette = true,
         long_message_to_split = true,
         inc_rename = true,

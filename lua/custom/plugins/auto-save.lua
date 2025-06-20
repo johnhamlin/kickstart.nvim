43753253c9f88cf9e931e207c5ffebaff54b0f2f
John Hamlin <johnhamlin@gmail.com>
2025-06-20 13:06:14 -0400

    Disable auto-save

--- lua/custom/plugins/auto-save.lua
+++ lua/custom/plugins/auto-save.lua
@@ -1,10 +1,12 @@
 return {
   {
-    "pocco81/auto-save.nvim",
-    event = { "BufReadPost", "BufNewFile" }, -- load the plugin for any editable buffer
+    enabled = false,
+    'pocco81/auto-save.nvim',
+    event = { 'BufReadPost', 'BufNewFile' }, -- load the plugin for any editable buffer
     opts = {
       -- Save whenever the current buffer loses focus
-      trigger_events = { "BufLeave" },
+      trigger_events = { 'BufLeave' },
     },
   },
-} 
+}
+

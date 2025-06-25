71a749051b31b6c939a0469ac33de8d0b2fbb6e7
John Hamlin <johnhamlin@gmail.com>
2025-06-25 12:45:02 -0400

    Remove task wiki

--- lua/custom/plugins/vimwiki.lua
+++ lua/custom/plugins/vimwiki.lua
@@ -18,28 +18,28 @@ return {
   ---------------------------------------------------------------------------
   -- ➋ Taskwiki
   ---------------------------------------------------------------------------
-  {
-    'tools-life/taskwiki',
-    event = 'VeryLazy',
-    ft = { 'vimwiki' },
-    dependencies = { 'vimwiki/vimwiki' },
-    init = function()
-      -- vim.g.mapleader = ' '
-      -- vim.g.maplocalleader = ' '
-      -- vim.g.taskwiki_maplocalleader = vim.g.maplocalleader
-
-      -- keep Taskwarrior DB somewhere version-controlled (optional)
-      vim.g.taskwiki_data_location = vim.fn.expand '~/Documents/task'
-
-      -- REMOVE the Markdown override; default markup matches Vimwiki headings
-      -- vim.g.taskwiki_markup_syntax = "markdown"
-
-      -- QoL tweaks
-      vim.g.taskwiki_disable_concealcursor = 1 -- keep filters visible in insert mode
-      vim.g.taskwiki_dont_fold = "yes"
-      vim.g.taskwiki_dont_preserve_folds = "yes"
-    end,
-  },
+  -- {
+  --   'tools-life/taskwiki',
+  --   event = 'VeryLazy',
+  --   ft = { 'vimwiki' },
+  --   dependencies = { 'vimwiki/vimwiki' },
+  --   init = function()
+  --     -- vim.g.mapleader = ' '
+  --     -- vim.g.maplocalleader = ' '
+  --     -- vim.g.taskwiki_maplocalleader = vim.g.maplocalleader
+  --
+  --     -- keep Taskwarrior DB somewhere version-controlled (optional)
+  --     vim.g.taskwiki_data_location = vim.fn.expand '~/Documents/task'
+  --
+  --     -- REMOVE the Markdown override; default markup matches Vimwiki headings
+  --     -- vim.g.taskwiki_markup_syntax = "markdown"
+  --
+  --     -- QoL tweaks
+  --     vim.g.taskwiki_disable_concealcursor = 1 -- keep filters visible in insert mode
+  --     vim.g.taskwiki_dont_fold = 'yes'
+  --     vim.g.taskwiki_dont_preserve_folds = 'yes'
+  --   end,
+  -- },
   --   -- only load when a Vimwiki buffer is opened
   --   ft = { 'vimwiki' },
   --
@@ -75,23 +75,23 @@ return {
   -- -- ---------------------------------------------------------------------------
   -- -- -- ➌ (optional) ANSI colours in Taskwarrior charts
   -- -- ---------------------------------------------------------------------------
-  { 'powerman/vim-plugin-AnsiEsc', cmd = 'AnsiEsc', ft = { 'vimwiki' } },
+  -- { 'powerman/vim-plugin-AnsiEsc', cmd = 'AnsiEsc', ft = { 'vimwiki' } },
 
   -- -- ---------------------------------------------------------------------------
   -- -- -- ➍ (optional) Telescope-powered Taskwarrior search
   -- -- ---------------------------------------------------------------------------
-  {
-    'nvim-telescope/telescope.nvim',
-    optional = true,
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-      {
-        '<leader>st',
-        function()
-          require('telescope.builtin').command_history { default_text = 'task ' }
-        end,
-        desc = 'Search Taskwarrior reports',
-      },
-    },
-  },
+  -- {
+  --   'nvim-telescope/telescope.nvim',
+  --   optional = true,
+  --   dependencies = { 'nvim-lua/plenary.nvim' },
+  --   keys = {
+  --     {
+  --       '<leader>st',
+  --       function()
+  --         require('telescope.builtin').command_history { default_text = 'task ' }
+  --       end,
+  --       desc = 'Search Taskwarrior reports',
+  --     },
+  --   },
+  -- },
 }

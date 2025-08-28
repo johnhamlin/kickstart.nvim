a7db1489be4706545ad56b627dd213d43cbb1838
John Hamlin <johnhamlin@gmail.com>
2025-08-28 17:34:17 -0400

    More TS QoL

--- init.lua
+++ init.lua
@@ -1071,7 +1071,25 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'swift' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'swift',
+        'javascript',
+        'typescript',
+        'tsx',
+        'json',
+        'css',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
---
+++ lua/custom/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,7 @@
+return {
+  'windwp/nvim-ts-autotag',
+  event = 'VeryLazy',
+  config = function()
+    require('nvim-treesitter.configs').setup { autotag = { enable = true } }
+  end,
+}
